8c9b54ccecd5dce3cb73cb69f83681ef3440e4be
NelsonBlakeN <nelson.blaken@gmail.com>
2016-06-17 21:14:32 -0500

    Functional version of the bot
    All logic for choosing the right selection of gifs, and one of the selection at random, included. Includes folders that are currently empty.

--- ProdBot.py
+++ ProdBot.py
@@ -24,14 +24,37 @@ response = requests.get('http://api.openweathermap.org/data/2.5/forecast/daily?q
 today = response['cnt']-1
 r_day_list = response['list']
 r_tod = r_day_list[today]
-r_tod_dt = r_tod['dt']
 
-x = random.randint(10,50)
-x = int(round(x/10))
+gif_choice = random.randint(10,50)
+gif_choice = int(round(gif_choice/10))
 
-path = "/home/pi/Weather\ Gifs/hot" + str(x) + ".gif"
+tod_weather_id = r_tod['weather'][0]['id']
+
+clear_sky = 800
+few_clouds = 801
+
+if tod_weather_id is clear_sky or tod_weather_id is few_clouds:
+    tod_temp_k = r_tod['temp']['day']
+    tod_temp_f = 1.8*(tod_temp_k-273.15)+32
+
+    if tod_temp_f >= 85:
+        path = "/home/pi/Weather\ Gifs/hot/hot" + str(gif_choice) + ".gif"
+    elif tod_temp_f <= 84 and tod_temp_f >= 60:
+        path = "/home/pi/Weather\ Gifs/nice_day/nice" + str(gif_choice) + ".gif"
+    elif tod_temp_f < 60:
+        path = "/home/pi/Weather\ Gifs/cold/cold" + str(gif_choice) + ".gif"
+elif tod_weather_id > 200 and tod_weather_id < 600:
+    path = "/home/pi/Weather\ Gifs/rainy/rainy" + str(gif_choice) + ".gif"
+elif tod_weather_id < 700 and tod_weather_id >= 600:
+    path = "/home/pi/Weather\ Gifs/snowy/snowy" + str(gif_choice) + ".gif"
+elif tod_weather_id <= 804 and tod_weather_id >= 802:
+    path = "/home/pi/Weather\ Gifs/cloudy/cloudy" + str(gif_choice) + ".gif"
+else:
+    path = None
+    t_api.update_status(status="No gif today folks, check back tomorrow for a new one!")
+
+if path:
 gif = open(path, 'rb')
 response = t_api.upload_media(media=gif)
-# t_api.update_status(status="Today is " + str(datetime.datetime.fromtimestamp(t_tod_dt)))
 
 t_api.update_status(media_ids=[response['media_ids']])
