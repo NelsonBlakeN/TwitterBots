6e3e60635233757b1a1e5410cd856e50c4d2c656
NelsonBlakeN <nelson.blaken@gmail.com>
2017-01-04 23:05:57 -0600

    Adding SMS functionality. The bot is now able to text updates.

--- FollowCounter/follower_counter.py
+++ FollowCounter/follower_counter.py
@@ -2,10 +2,20 @@ import requests
 import oauth2 as oauth
 import json
 
+from twilio.rest import TwilioRestClient
+
+# Twitter API requirements
 CONSUMER_KEY    = 'lygeb5MmWPyTLZidxgSXHdFno'
 CONSUMER_SECRET = 'JmgS5lUfUT0ZrB36hYQJ5cOINpuM4lOEnbBXwESAefVOGslJG8'
 ACCESS_TOKEN    = '737403682111643648-PIeyxkm7XPj9ARta66Ly6AQJGQJPEz3'
 ACCESS_SECRET   = 'yy8qFkJ0TJhmx6DrfVs4FWneBwdyqxVRq2gfxGTZ0CtrL'
+
+# Twilio API Requirements
+ACCOUNT_SID = 'ACe9a7f6ee6de1a5aa66fb68c386ec530f'
+AUTH_TOKEN  = '9be322a9d2603be029567bd0c2dee7b4'
+twilio_client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+MY_TWIL_NUM = "+18177765044"
+MY_CELL_NUM = "+18173419172"
 
 consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
 access_token = oauth.Token(key=ACCESS_TOKEN, secret=ACCESS_SECRET)
@@ -15,7 +25,7 @@ newfile = 'old_follow_count.txt'
 
 def old_follow_count():
     file = open(newfile, 'r')
-    return file.readline()
+    return int(file.readline())
 
 def current_follow_count():
     response, data = client.request("https://api.twitter.com/1.1/followers/ids.json?cursor=-1&screen_name=frostedbIakes&count=5000")
@@ -26,21 +36,21 @@ def current_follow_count():
     c = len(d['ids'])
     return c
 
-print "You have", old_follow_count(), "followers"
-old_count = 0
+def send_alert(d):
+    if(d < 0):
+        body = "Blake - You lost " + str(abs(d)) + " followers today."
+    elif(d > 0):
+        body = "Blake - You gained " + str(d) + " followers today."
+    message = twilio_client.messages.create(body=body, from_=MY_TWIL_NUM, to=MY_CELL_NUM)
+
+old_count = old_follow_count()
 if old_count is None:
     old_count = 0;
 current_count = current_follow_count()
 diff = current_count - old_count
-if(diff < 0):
-    print "You lost ", abs(diff), " followers!"
-elif(diff > 0):
-    print "You gained", diff, "followers!"
-else:
-    print "No change"
 
-with open(newfile, 'w') as fi:
-    print "Writing to file..."
-    fi.write(str(current_count))
+send_alert(diff)
 
-
+with open(newfile, 'w') as f:
+    f.truncate()
+    f.write(str(current_count))
