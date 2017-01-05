7b291f937e97ee5ffcf432ecb1f5156e3f02e8f5
NelsonBlakeN <nelson.blaken@gmail.com>
2017-01-04 21:28:26 -0600

    Changed approach to file I/O. Much more readable and efficient this way.

--- FollowCounter/follower_counter.py
+++ FollowCounter/follower_counter.py
@@ -1,9 +1,6 @@
 import requests
 import oauth2 as oauth
-import sys
-
 import json
-import pickle
 
 CONSUMER_KEY    = 'lygeb5MmWPyTLZidxgSXHdFno'
 CONSUMER_SECRET = 'JmgS5lUfUT0ZrB36hYQJ5cOINpuM4lOEnbBXwESAefVOGslJG8'
@@ -14,22 +11,11 @@ consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
 access_token = oauth.Token(key=ACCESS_TOKEN, secret=ACCESS_SECRET)
 client = oauth.Client(consumer, access_token)
 
-newfile = 'mypickle.pk'
-
-# response, data = client.request("https://api.twitter.com/1.1/followers/ids.json?cursor=-1&screen_name=frostedbIakes&count=5000")
-# # Convert 'data' from string to dictionary
-# json_string = data.replace("'", "\"")
-# d = json.loads(json_string)
-
-# print "Followers: ", len(d['ids'])
+newfile = 'old_follow_count.txt'
 
 def old_follow_count():
-    file = open(newfile, 'w+b')
-    # with open(newfile, 'rb') as fi:
-    #     old_count = pickle.load(fi)
-    # oc = pickle.load(open(newfile, 'rb'))
-    oc = pickle.load(file)
-    return oc
+    file = open(newfile, 'r')
+    return file.readline()
 
 def current_follow_count():
     response, data = client.request("https://api.twitter.com/1.1/followers/ids.json?cursor=-1&screen_name=frostedbIakes&count=5000")
@@ -40,19 +26,21 @@ def current_follow_count():
     c = len(d['ids'])
     return c
 
-old_count = old_follow_count()
+print "You have", old_follow_count(), "followers"
+old_count = 0
 if old_count is None:
     old_count = 0;
 current_count = current_follow_count()
-diff = current_count
+diff = current_count - old_count
 if(diff < 0):
     print "You lost ", abs(diff), " followers!"
 elif(diff > 0):
-    print "You gained", diff, " followers!"
+    print "You gained", diff, "followers!"
 else:
     print "No change"
 
-with open(newfile, 'wb') as fi:
-    pickle.dump(current_count, fi)
+with open(newfile, 'w') as fi:
+    print "Writing to file..."
+    fi.write(str(current_count))
 
 
