c8d67f1241ba86606bd2346e48f9d3cf429b8e4f
NelsonBlakeN <nelson.blaken@gmail.com>
2017-01-05 00:37:50 -0600

    Hid API keys by moving them to a wrapper file that is imported. This wrapper also contains client objects that could potentially be used in future projects, so it is advantagious to abstract them to their own file.

--- FollowCounter/follower_counter.py
+++ FollowCounter/follower_counter.py
@@ -1,25 +1,16 @@
 import requests
 import oauth2 as oauth
 import json
+import twitter_creds as twit_c
+import twilio_creds as twil_c
 
-from twilio.rest import TwilioRestClient
-
-# Twitter API requirements
-CONSUMER_KEY    = 'lygeb5MmWPyTLZidxgSXHdFno'
-CONSUMER_SECRET = 'JmgS5lUfUT0ZrB36hYQJ5cOINpuM4lOEnbBXwESAefVOGslJG8'
-ACCESS_TOKEN    = '737403682111643648-PIeyxkm7XPj9ARta66Ly6AQJGQJPEz3'
-ACCESS_SECRET   = 'yy8qFkJ0TJhmx6DrfVs4FWneBwdyqxVRq2gfxGTZ0CtrL'
+# Twitter API object
+twitter = twit_c.client
 
 # Twilio API Requirements
-ACCOUNT_SID = 'ACe9a7f6ee6de1a5aa66fb68c386ec530f'
-AUTH_TOKEN  = '9be322a9d2603be029567bd0c2dee7b4'
-twilio_client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
-MY_TWIL_NUM = "+18177765044"
-MY_CELL_NUM = "+18173419172"
-
-consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
-access_token = oauth.Token(key=ACCESS_TOKEN, secret=ACCESS_SECRET)
-client = oauth.Client(consumer, access_token)
+twilio = twil_c.client
+MY_TWIL_NUM = twil_c.MY_TWIL_NUM
+MY_CELL_NUM = twil_c.MY_CELL_NUM
 
 newfile = 'old_follow_count.txt'
 
@@ -41,7 +32,7 @@ def old_follow_count():
     return int(file.readline())
 
 def current_follow_count():
-    response, data = client.request("https://api.twitter.com/1.1/followers/ids.json?cursor=-1&screen_name=frostedbIakes&count=5000")
+    response, data = twitter.request("https://api.twitter.com/1.1/followers/ids.json?cursor=-1&screen_name=frostedbIakes&count=5000")
     # Convert 'data' from string to dictionary
     json_string = data.replace("'", "\"")
     d = json.loads(json_string)
