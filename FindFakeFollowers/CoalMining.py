07f8bceffdfbb0771ee7fa4dba10196e55e70721
NelsonBlakeN <nelson.blaken@gmail.com>
2017-01-31 16:38:06 -0600

    Cleaned up debugging statements. Successfully gathering all followers, now must find a way to get each follow count without hitting the rate limit. Possibly pausing between each ~150 calls

--- FindFakeFollowers/CoalMining.py
+++ FindFakeFollowers/CoalMining.py
@@ -8,12 +8,11 @@ def main():
 
 def follower_list():
     # Obtain correct auth token
-    user, pw = get_creds()
+    user, pw = get_creds(True)
     insta = InstagramAPI(user, pw)
     insta.login()
 
     # Get a list of all followers
-    # insta.getSelfUserFollowers()
     insta.searchUsername("egcarlin")
     insta_id = insta.LastJson['user']['pk']
 
@@ -21,40 +20,39 @@ def follower_list():
     followers = insta.LastJson['users']
     max_id = insta.LastJson['next_max_id']
     while insta.LastResponse.status_code != 429:
-        insta.getUserFollowers(insta_id, max_id)
-        if insta.LastJson['next_max_id']:
+        try:
+            insta.getUserFollowers(insta_id, max_id)
             followers += insta.LastJson['users']
             max_id = insta.LastJson['next_max_id']
+        except KeyError, e:
+            break
     if insta.LastResponse.status_code == 429:
         print "Hit rate limit when collection followers."
-        print len(followers)
         return
-    # pprint(followers)
+
+    print "Finished collecting all", len(followers), "followers"
 
     # Check if any are following less than 5 people
     # (and that one is the target, for redundancy)
-    count = 1
     for u in followers:
         print u['username']
         insta.searchUsername(u['username']) # Rate limit caps at 100 / hr
         if insta.LastResponse.status_code == 429:
             print "Hit rate limit when searching usernames."
-            print count
             return
-        count += 1
-        # user = insta.LastJson['user']
-        pprint(user)
-        print "\n"
+        user = insta.LastJson['user']
         if user['following_count'] < 5:
             # If a suspicious account is found, report/block it
             # insta.block(user['pk'])
             print "Suspicious account found"
 
-def get_creds():
-    # file = open(FILENAME, 'r')
-    # return file.readline()
-    # return "4307299325.7efde7b.58681b38682641d589a4f49872b8aa45"
-    return "blakenelson19", "Tamu2019"
+def get_creds(test=False):
+    if False:
+        file = open(FILENAME, 'r')
+        return file.readline()
+        # return "4307299325.7efde7b.58681b38682641d589a4f49872b8aa45"
+    else:
+        return "blakenelson19", "Tamu2019"
 
 if __name__ == "__main__":
     main()
