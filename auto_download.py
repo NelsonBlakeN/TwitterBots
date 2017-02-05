c806c07ffea2d121a03a54394db02f15c90509cb
NelsonBlakeN <nelson.blaken@gmail.com>
2017-02-04 19:08:43 -0600

    Added email functionality to the program. It will now emial me every time it runs with a status report of whether anything was downloaded or not. This serves as both an informative service, but also as a way to let me know things are working, since it is being run as a scheduled Windows task.

--- auto_download.py
+++ auto_download.py
@@ -1,5 +1,8 @@
 import urllib2
 import re
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 # Use regex to find all links. Keep increading number in pattern (HW1, 2, etc)
 # up to 5 until there is no match. If there is no match before 5, download the prev
@@ -8,6 +11,11 @@ import re
 # Separate the homework and programming assignments
 
 # Save what has already been downloaded, download the rest
+
+html = """<p>This notification is to let you know the AutoDownload program ran today.<br><br>
+    The following is the result that was collected:<br>
+    %s
+"""
 
 def download_csce_hw():
     assignment_url = "http://faculty.cs.tamu.edu/schaefer/teaching/221_Spring2017/"
@@ -22,8 +30,7 @@ def download_csce_hw():
     pattern = "(PA|HW)(" + str(x) + ").doc"
     assignments = [m.start() for m in re.finditer(pattern, html)]
     if not assignments:
-        print "No new assignments have been posted."
-        return
+        return "No new assignments have been posted."
     names = []
     for a in assignments:   # Generalized, in case only HW/PA for that number has been posted.
         name = html[a:a+7]
@@ -34,6 +41,7 @@ def download_csce_hw():
             pa = (name, "Programming Assignments")
             names.append(pa)
 
+    result = ", ".join(names)
     for n in names:
         doc_name = n[0]
         doc_url = "Assignments/" + doc_name
@@ -44,9 +52,35 @@ def download_csce_hw():
         with open(win_path, 'wb') as output:
             output.write(doc.read())
 
-    with open(filename, 'w') as output: # Save the last assignment posted in the sequence
+    with open(filename, 'w') as output: # Save the last assignment posted in the sequence to the text file
         output.truncate()
         output.write(str(x))
 
+    return result + " has been posted and saved to your path."
+
+def send_email(cs_result = ""):
+    email = ""
+    password = ""
+    with open("email_creds.txt", 'r') as e:
+        email = e.readline()
+        password = e.readline()
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "AutoDownload Notification"
+    msg['From'] = email
+    msg['To'] = "blake@blake-n-nelson.com"
+
+    body = MIMEText(html % cs_result, 'html')
+    msg.attach(body)
+
+    mail = smtplib.SMTP('smtp.gmail.com', 587)
+    mail.ehlo()
+    mail.starttls()
+    mail.login(email, password)
+    mail.sendmail(email, "blake@blake-n-nelson.com", msg.as_string())
+    mail.quit()
+
+
 if __name__ == "__main__":
-    download_csce_hw()
+    r1 = download_csce_hw()
+    send_email(r1)
