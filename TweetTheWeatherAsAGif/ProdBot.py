92cc33287163406742e3aefc852476c401d7dda8
NelsonBlakeN <nelson.blaken@gmail.com>
2017-12-26 22:41:47 -0600

    Corrected library issues and basic functionality (no schedule)

--- TweetTheWeatherAsAGif/ProdBot.py
+++ TweetTheWeatherAsAGif/ProdBot.py
@@ -1,4 +1,5 @@
 import sys
+sys.path.append("/home/pi/Bots")
 import random
 import logging
 
@@ -6,17 +7,9 @@ import logging
 import requests
 from datetime import datetime, time
 
-#Twitter API imports
-from twython import Twython
-
-# Twitter Keys/API
-CONSUMER_KEY = 'lygeb5MmWPyTLZidxgSXHdFno'
-CONSUMER_SECRET = 'JmgS5lUfUT0ZrB36hYQJ5cOINpuM4lOEnbBXwESAefVOGslJG8'
-ACCESS_TOKEN = '737403682111643648-PIeyxkm7XPj9ARta66Ly6AQJGQJPEz3'
-ACCESS_SECRET = 'yy8qFkJ0TJhmx6DrfVs4FWneBwdyqxVRq2gfxGTZ0CtrL'
-
-# Twitter API Request object
-t_api = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
+#Twitter API imports and objects
+from TwitterLib import WeatherTwitter
+twitter = WeatherTwitter()
 
 # Logging tool (setup)
 current_year = datetime.now().isocalendar()[0]
@@ -27,73 +20,80 @@ logging.basicConfig(filename=FILENAME, level=logging.INFO)
 logger = logging.getLogger('WeatherGifBot')
 
 #Weather API Key/calls
-key = "e8b29a056adebaa04d8f733f3a82897f"
+key = "e8b29a056adebaa04d8f733f3a82897f"    # API Key
+# Multiday, daily weather forecast request (city=Dallas, # of days=2)
 response = requests.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=Dallas&cnt=2&APPID='+key).json()
 
 #Obtaining the appropriate data drom the weather api response
-today = response['cnt']-1
-r_day_list = response['list']
-r_tod = r_day_list[today]
+today = response['cnt']-1                       # Index of desired day (today)
+list_of_forecasts = response['list']            # List of forecasted days
+forecast = list_of_forecasts[today]             # Forecast of desired day
 
-gif_choice = random.randint(1,14)
+gif_choice = random.randint(1,14)               # Random gif for the forecast
 
-tod_weather_id = r_tod['weather'][0]['id']
+tod_weather_id = forecast['weather'][0]['id']   # Today's weather ID (categorizes weather)
 
+# Define some weather IDs
 clear_sky = 800
 few_clouds = 801
 xtreme = {'900': 'tornado',
-          '901': 'tropical storm',
-          '902': 'hurricane',
-          '906': 'hail',
-          '961': 'violent storm',
-          '962': 'hurricane'
-          }
+        '901': 'tropical storm',
+        '902': 'hurricane',
+        '906': 'hail',
+        '961': 'violent storm',
+        '962': 'hurricane'
+        }
 
+# Obtain current forecast (in case of emergency weather)
 current_forecast = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=76248,us&APPID='+key)
 
+# Obtain current time and define non-emergency schedule
 now = datetime.now().time()
 sch_upper_end = time(7, 05)
 sch_lower_end = time(6, 55)
 
-if sch_upper_end <= now or now <= sch_lower_end:
-    if str(current_forecast) in xtreme or str(tod_weather_id) in xtreme:
-          t_api.update_status(status="ALERT: Extreme weather in your area, seek shelter and adhere to your local news network.")
+# If outisde non-emergnecy schedule, check for emergency weather
+# if now >= sch_upper_end or now <= sch_lower_end:
+#     if str(current_forecast) in xtreme or str(tod_weather_id) in xtreme:
+#         twitter.post_tweet(text="ALERT: Extreme weather in your area, seek shelter and adhere to your local news network.")
 
+# else:
+# Logging data
+logger.info('Today is ' + str(datetime.now()))
+logger.info("Gif number: " + str(gif_choice))
+logger.info("Today's weather ID: " + str(tod_weather_id))
 
+# Determine weather category based on temperature
+if tod_weather_id == clear_sky or tod_weather_id == few_clouds:
+    tod_temp_k = forecast['temp']['day']        # Today's temperature (Kelvin)
+    tod_temp_f = 1.8*(tod_temp_k-273.15)+32     # Convert kelvin to Fahrenheit
+
+    if tod_temp_f >= 85:
+        logger.info("HOT")
+        path = "/home/pi/WeatherGifs/hot/hot" + str(gif_choice) + ".gif"
+    elif tod_temp_f <= 84 and tod_temp_f >= 60:
+        logger.info("NICE DAY")
+        gif_choice = random.randint(1,11)	# There are 11 nice day gifs, so we reroll the choice
+        path = "/home/pi/WeatherGifs/nice_day/nice" + str(gif_choice) + ".gif"
+    elif tod_temp_f < 60:
+        logger.info("COLD")
+        path = "/home/pi/WeatherGifs/cold/cold" + str(gif_choice) + ".gif"
+
+# Determine weather category based on weather ID
+elif tod_weather_id > 200 and tod_weather_id < 600:
+    logger.info("RAINY")
+    path = "/home/pi/WeatherGifs/rainy/rainy" + str(gif_choice) + ".gif"
+elif tod_weaher_id < 700 and tod_weather_id >= 600:
+    path = "/home/pi/WeatherGifs/snowy/snowy" + str(gif_choice) + ".gif"
+elif tod_weather_id <= 804 and tod_weather_id >= 802:
+    path = "/home/pi/WeatherGifs/cloudy/cloudy" + str(gif_choice) + ".gif"
 else:
-	logger.info('Today is ' + str(datetime.now()))
-	logger.info("Gif number: " + str(gif_choice))
-	logger.info("Today's weather ID: " + str(tod_weather_id))
+    # Something went wrong, weather category could not be determined
+    path = None
+    twitter.post_tweet(text="No gif today folks, check back tomorrow for a new one!")
 
-	if tod_weather_id == clear_sky or tod_weather_id == few_clouds:
-		tod_temp_k = r_tod['temp']['day']
-    		tod_temp_f = 1.8*(tod_temp_k-273.15)+32
+if path:
+    # Tweet the  weather gif
+    twitter.post_tweet(media=path)
 
-    		if tod_temp_f >= 85:
-			logger.info("HOT")
-        		path = "/home/pi/Weather Gifs/hot/hot" + str(gif_choice) + ".gif"
-    		elif tod_temp_f <= 84 and tod_temp_f >= 60:
-			logger.info("NICE DAY")
-			gif_choice = random.randint(1,11)	# There are 11 nice day gifs, so we reroll the choice
-        		path = "/home/pi/Weather Gifs/nice_day/nice" + str(gif_choice) + ".gif"
-	    	elif tod_temp_f < 60:
-			logger.info("COLD")
-        		path = "/home/pi/Weather Gifs/cold/cold" + str(gif_choice) + ".gif"
-	elif tod_weather_id > 200 and tod_weather_id < 600:
-		logger.info("RAINY")
-    		path = "/home/pi/Weather Gifs/rainy/rainy" + str(gif_choice) + ".gif"
-	elif tod_weather_id < 700 and tod_weather_id >= 600:
-   		path = "/home/pi/Weather Gifs/snowy/snowy" + str(gif_choice) + ".gif"
-	elif tod_weather_id <= 804 and tod_weather_id >= 802:
-   		path = "/home/pi/Weather Gifs/cloudy/cloudy" + str(gif_choice) + ".gif"
-	else:
-    		path = None
-    		t_api.update_status(status="No gif today folks, check back tomorrow for a new one!")
-
-	if path:
-    		gif = open(path, 'rb')
-    		response = t_api.upload_media(media=gif)
-
-    		t_api.update_status(media_ids=[response['media_id']])
-
-	logger.info("\n \n ************************** \n")
+logger.info("\n \n ************************** \n")
