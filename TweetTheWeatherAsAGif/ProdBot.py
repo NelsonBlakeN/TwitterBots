de6b1cc23600e0465b6ef3d7a04835bf32966460
NelsonBlakeN <nelson.blaken@gmail.com>
2017-12-26 22:54:33 -0600

    Merging test branch changes

--- TweetTheWeatherAsAGif/ProdBot.py
+++ TweetTheWeatherAsAGif/ProdBot.py
@@ -53,47 +53,48 @@ sch_upper_end = time(7, 05)
 sch_lower_end = time(6, 55)
 
 # If outisde non-emergnecy schedule, check for emergency weather
-# if now >= sch_upper_end or now <= sch_lower_end:
-#     if str(current_forecast) in xtreme or str(tod_weather_id) in xtreme:
-#         twitter.post_tweet(text="ALERT: Extreme weather in your area, seek shelter and adhere to your local news network.")
+if now >= sch_upper_end or now <= sch_lower_end:
+    if str(current_forecast) in xtreme or str(tod_weather_id) in xtreme:
+        twitter.post_tweet(text="ALERT: Extreme weather in your area, seek shelter and adhere to your local news network.")
 
-# else:
-# Logging data
-logger.info('Today is ' + str(datetime.now()))
-logger.info("Gif number: " + str(gif_choice))
-logger.info("Today's weather ID: " + str(tod_weather_id))
-
-# Determine weather category based on temperature
-if tod_weather_id == clear_sky or tod_weather_id == few_clouds:
-    tod_temp_k = forecast['temp']['day']        # Today's temperature (Kelvin)
-    tod_temp_f = 1.8*(tod_temp_k-273.15)+32     # Convert kelvin to Fahrenheit
-
-    if tod_temp_f >= 85:
-        logger.info("HOT")
-        path = "/home/pi/WeatherGifs/hot/hot" + str(gif_choice) + ".gif"
-    elif tod_temp_f <= 84 and tod_temp_f >= 60:
-        logger.info("NICE DAY")
-        gif_choice = random.randint(1,11)	# There are 11 nice day gifs, so we reroll the choice
-        path = "/home/pi/WeatherGifs/nice_day/nice" + str(gif_choice) + ".gif"
-    elif tod_temp_f < 60:
-        logger.info("COLD")
-        path = "/home/pi/WeatherGifs/cold/cold" + str(gif_choice) + ".gif"
-
-# Determine weather category based on weather ID
-elif tod_weather_id > 200 and tod_weather_id < 600:
-    logger.info("RAINY")
-    path = "/home/pi/WeatherGifs/rainy/rainy" + str(gif_choice) + ".gif"
-elif tod_weaher_id < 700 and tod_weather_id >= 600:
-    path = "/home/pi/WeatherGifs/snowy/snowy" + str(gif_choice) + ".gif"
-elif tod_weather_id <= 804 and tod_weather_id >= 802:
-    path = "/home/pi/WeatherGifs/cloudy/cloudy" + str(gif_choice) + ".gif"
+# Non-emergency schedule
 else:
-    # Something went wrong, weather category could not be determined
-    path = None
-    twitter.post_tweet(text="No gif today folks, check back tomorrow for a new one!")
+    # Logging data
+    logger.info('Today is ' + str(datetime.now()))
+    logger.info("Gif number: " + str(gif_choice))
+    logger.info("Today's weather ID: " + str(tod_weather_id))
 
-if path:
-    # Tweet the  weather gif
-    twitter.post_tweet(media=path)
+    # Determine weather category based on temperature
+    if tod_weather_id == clear_sky or tod_weather_id == few_clouds:
+        tod_temp_k = forecast['temp']['day']        # Today's temperature (Kelvin)
+        tod_temp_f = 1.8*(tod_temp_k-273.15)+32     # Convert kelvin to Fahrenheit
 
-logger.info("\n \n ************************** \n")
+        if tod_temp_f >= 85:
+            logger.info("HOT")
+            path = "/home/pi/WeatherGifs/hot/hot" + str(gif_choice) + ".gif"
+        elif tod_temp_f <= 84 and tod_temp_f >= 60:
+            logger.info("NICE DAY")
+            gif_choice = random.randint(1,11)	# There are 11 nice day gifs, so we reroll the choice
+            path = "/home/pi/WeatherGifs/nice_day/nice" + str(gif_choice) + ".gif"
+        elif tod_temp_f < 60:
+            logger.info("COLD")
+            path = "/home/pi/WeatherGifs/cold/cold" + str(gif_choice) + ".gif"
+
+    # Determine weather category based on weather ID
+    elif tod_weather_id > 200 and tod_weather_id < 600:
+        logger.info("RAINY")
+        path = "/home/pi/WeatherGifs/rainy/rainy" + str(gif_choice) + ".gif"
+    elif tod_weaher_id < 700 and tod_weather_id >= 600:
+        path = "/home/pi/WeatherGifs/snowy/snowy" + str(gif_choice) + ".gif"
+    elif tod_weather_id <= 804 and tod_weather_id >= 802:
+        path = "/home/pi/WeatherGifs/cloudy/cloudy" + str(gif_choice) + ".gif"
+    else:
+        # Something went wrong, weather category could not be determined
+        path = None
+        twitter.post_tweet(text="No gif today folks, check back tomorrow for a new one!")
+
+    if path:
+        # Tweet the  weather gif
+        twitter.post_tweet(media=path)
+
+    logger.info("\n \n ************************** \n")
