40299514cf5953339bcad2cba954bd91193415ea
Blake.Nelson <blake.nelson@tamu.edu>
2018-05-06 21:45:18 -0500

    Adding custon library to repo, but put keys in a separate file that is ignored, for security reasons. Also included temp file functionality in the event of logging failure, missing files, etc

--- TweetTheWeatherAsAGif/ProdBot.py
+++ TweetTheWeatherAsAGif/ProdBot.py
@@ -1,30 +1,55 @@
+# These libs are assumed to be always included
+import sys
+import os
+from tempfile import NamedTemporaryFile
+
+temp_file = NamedTemporaryFile(delete=False)
+
 try:
     import logging
-    import sys
+    from datetime import datetime, time
 except Exception as e:
-    print("ERROR sys/logging import failed: {}".format(e))
+    print("Something went wrong, see {} for details".format(temp_file.name))
+    temp_file.write("ERROR logging import failed: {}".format(e))
+    temp_file.close()
+    os.unlink(temp_file.name)
+    sys.exit()
 
-# Set up logging tool
-current_year = datetime.now().isocalendar()[0]
-current_week = datetime.now().isocalendar()[1]
-FILENAME = '/home/pi/Documents/logs/WeatherBotLogs/'+str(current_year)+'Week'+str(current_week)+'.log'
-logging.basicConfig(filename=FILENAME, level=logging.INFO)
-
-logger = logging.getLogger('WeatherGifBot')
+sys.path.append("..")
 
 try:
-    sys.path.append("/home/pi/Bots")
+    # Set up logging tool
+    current_year = datetime.now().isocalendar()[0]
+    current_week = datetime.now().isocalendar()[1]
+    FILENAME = '/home/pi/Documents/logs/WeatherBotLogs/'+str(current_year)+'Week'+str(current_week)+'.log'
+    logging.basicConfig(filename=FILENAME, level=logging.INFO)
+
+    logger = logging.getLogger('WeatherGifBot')
+except FileNotFoundError:
+    FILENAME = temp_file.name
+    logging.basicConfig(filename=FILENAME, level=logging.INFO)
+    logger = logging.getLogger('WeatherGifBot')
+except Exception as e:
+    print("Something went wrong, see {} for details".format(temp_file.name))
+    temp_file.write("ERROR log creation failed: {}".format(e))
+    temp_file.close()
+    os.unlink(temp_file.name)
+    os.exit()
+
+try:
+    # sys.path.append("/home/pi/Bots")
     import random
 
     #Weather API imports
     import requests
-    from datetime import datetime, time
 
     #Twitter API imports
     from TwitterLib import WeatherTwitter
 
 except Exception as e:
     logger.error("Import failed: {}".format(e))
+    # print("ERROR import failed: {}".format(e))
+    sys.exit()
 
 # Define constants
 SEPARATOR = "\n \n ************************** \n"
@@ -34,6 +59,14 @@ CURRENT_FORECAST_URL = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=
 
 CLEAR_SKY = 800     # Weather API code for a clear sky in forecast
 FEW_CLOUDS = 801    # Weather API code for some clouds in forecast
+RAIN_ID = 200       # Weather API code for rain
+SNOW_ID = 600       # Weather API code for snow
+SNOW_ID_TOP = 700   # Upper range of snow weather API code
+CLOUD_ID_TOP = 804  # Upper range of cloudy weather API code
+CLOUD_ID_BTM = 802  # Lower range of cloudy weather API code
+
+HOT_TEMP = 85
+COLD_TEMP = 60
 
 HOT_PATH = "/home/pi/WeatherGifs/hot/hot"               # Path to "hot" gifs
 NICE_DAY_PATH = "/home/pi/WeatherGifs/nice_day/nice"    # Path to "nice day" gifs
@@ -44,15 +77,14 @@ CLOUDY_PATH = "/home/pi/WeatherGifs/cloudy/cloudy"      # Path to "cloudy" gifs
 
 # Helper functions
 def terminate(msg=None, e=None):
-    logging.error(msg.format(e))
-    logging.error("Exiting.")
-    logging.info(SEPARATOR)
+    logger.error(msg.format(e))
+    logger.error("Exiting.")
+    logger.info(SEPARATOR)
     sys.exit()
 
 # Create necessary objects
 try:
     twitter = WeatherTwitter()
-    # TODO: Adapt this to use the library (on pi)
 
 except Exception as e:
     terminate("Failed to create necessary objects: {}", e)
@@ -60,7 +92,7 @@ except Exception as e:
 #Weather API Key/calls
 try:
     # Multiday, daily weather forecast request (city=Bryan, # of days=2)
-    response = requests.get(CURRENT_FORECAST_URL+key).json()
+    response = requests.get(CURRENT_FORECAST_URL+WEATHER_KEY).json()
 except Exception as e:
     terminate("Failed to make weather API calls: {}", e)
 
@@ -84,15 +116,14 @@ xtreme = {'900': 'tornado',
 
 # Obtain current forecast (in case of emergency weather)
 try:
-    current_forecast = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=77840,us&APPID='+key)
+    current_forecast = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=77840,us&APPID='+WEATHER_KEY)
 except Exception as e:
-    logging.error("Current forcast request failed: {}".format(e))
-    logging.info("Continuing.")
+    logger.error("Current forcast request failed: {}".format(e))
+    logger.info("Continuing.")
 
 # Obtain current time and define non-emergency schedule
-# TODO: Change time() to constants
 now = datetime.now().time()
-sch_upper_end = time(7, 05)
+sch_upper_end = time(7, 5)
 sch_lower_end = time(6, 55)
 
 # If outisde non-emergnecy schedule, check for emergency weather
@@ -109,32 +140,31 @@ else:
     logger.info("Today's weather ID: " + str(tod_weather_id))
 
     # Determine weather category based on temperature
-    # TODO: Remove magic numbers (temps, API codes)
     if tod_weather_id == CLEAR_SKY or tod_weather_id == FEW_CLOUDS:
         tod_temp_k = forecast['temp']['day']        # Today's temperature (Kelvin)
         tod_temp_f = 1.8*(tod_temp_k-273.15)+32     # Convert kelvin to Fahrenheit
 
-        if tod_temp_f >= 85:
+        if tod_temp_f >= HOT_TEMP:
             logger.info("HOT")
             path = HOT_PATH + str(gif_choice) + ".gif"
-        elif tod_temp_f <= 84 and tod_temp_f >= 60:
+        elif tod_temp_f < HOT_TEMP and tod_temp_f >= COLD_TEMP:
             logger.info("NICE DAY")
             # There are 11 nice day gifs, so we reroll the choice
             gif_choice = random.randint(1,11)
             path = NICE_DAY_PATH + str(gif_choice) + ".gif"
-        elif tod_temp_f < 60:
+        elif tod_temp_f < COLD_TEMP:
             logger.info("COLD")
             path = COLD_PATH + str(gif_choice) + ".gif"
 
     # Determine weather category based on weather ID
-    elif tod_weather_id > 200 and tod_weather_id < 600:
+    elif tod_weather_id > RAIN_ID and tod_weather_id < SNOW_ID:
         logger.info("RAINY")
         gif_choice = random.randint(1,12) # Due to size issues with some gifs, reroll
         path = RAINY_PATH + str(gif_choice) + ".gif"
-    elif tod_weaher_id < 700 and tod_weather_id >= 600:
+    elif tod_weather_id < SNOW_ID_TOP and tod_weather_id >= SNOW_ID:
         logger.info("SNOWY")
         path = SNOWY_PATH + str(gif_choice) + ".gif"
-    elif tod_weather_id <= 804 and tod_weather_id >= 802:
+    elif tod_weather_id <= CLOUD_ID_TOP and tod_weather_id >= CLOUD_ID_BTM:
         logger.info("CLOUDY")
         gif_choice = random.randint(2, 11) # Some gifs were removed due to media limits. Rerolling without renaming gifs
         path = CLOUDY_PATH + str(gif_choice) + ".gif"
@@ -144,7 +174,7 @@ else:
         try:
             twitter.post_tweet(text="No gif today folks, check back tomorrow for a new one!")
         except Exception as e:
-            logging.error("Default tweet failed to post: {}".format(e))
+            logger.error("Default tweet failed to post: {}".format(e))
 
     if path:
         # Tweet the weather gif
---
+++ TwitterLib.py
@@ -0,0 +1,61 @@
+import oauth2 as oauth
+import requests
+import simplejson
+from pprint import pprint
+
+# Included in .gitignore to preserve integrity of keys
+from TwitterKeys import *
+
+def json_data(data, index=None):
+	# Converts data string into a dictionary, and returns the desired entry
+	try:
+		data_dict = simplejson.loads(data)
+		if index is not None:
+			return data_dict[index]
+		else:
+			return data_dict
+	except KeyError as e:
+		return ""
+
+class PersonalTwitter:
+
+	# Constructor
+	def __init__(self):
+		CONSUMER_KEY = CONSUMER_KEY_PT
+		CONSUMER_SECRET = CONSUMER_SECRET_PT
+		ACCESS_TOKEN = ACCESS_TOKEN_PT
+		ACCESS_SECRET = ACCESS_SECRET_PT
+
+		consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+		access_token = oauth.Token(key=ACCESS_TOKEN, secret=ACCESS_SECRET)
+		self.client = oauth.Client(consumer, access_token)
+
+	# Get current follower count and list of followers
+	def get_followers(self):
+		response, data = self.client.request("https://api.twitter.com/1.1/followers/ids.json?cursor=-1&screen_name=frostedbIakes&count=5000")
+
+		# Convert 'data' from string to dictionary
+		current_follow_list = json_data(data, "ids")
+		follower_count = len(current_follow_list)
+		return follower_count, current_follow_list
+
+	# Returns the @ name (screen name) and user name of the given user
+	def get_user_name(self, user_id):
+		response, data = self.client.request("https://api.twitter.com/1.1/users/show.json?user_id="+str(user_id))
+		screen_name = json_data(data, "screen_name")
+		name = json_data(data, "name")
+
+		return (screen_name, name)
+
+class WeatherTwitter:
+
+	# Constructor
+	def __init__(self):
+		CONSUMER_KEY = CONSUMER_KEY_WT
+		CONSUMER_SECRET = CONSUMER_KEY_WT
+		ACCESS_TOKEN = ACCESS_TOKEN_WT
+		ACCESS_SECRET = ACCESS_SECRET_WT
+
+		consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+		access_token = oauth.Token(key=ACCESS_TOKEN, secret=ACCESS_SECRET)
+		self.client = oauth.Client(consumer, access_token)
